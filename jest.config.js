0bf516c7de3988df91c3e31b892d7711916fac2a
Bogdan Petkovic <bogdanpetkovic@outlook.com>
2023-05-18 15:02:05 +0200

    chore: jest setup

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  testEnvironment: 'node',
+  roots: ['<rootDir>'],
+  testMatch: ['**/*\\.(test|spec)\\.ts'],
+  transform: {
+    '^.+\\.ts$': 'ts-jest',
+  },
+  maxConcurrency: 4,
+  maxWorkers: 4,
+};
